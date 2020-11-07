a227e68916f9514d1e5faa60ee3d4b7fa114be62
Jonathan Borzilleri <jonathan@borzilleri.net>
2020-11-07 15:43:59 -0800

    Add timezone support to admin panel

--- client/js/admin.js
+++ client/js/admin.js
@@ -1,16 +1,21 @@
 import html from "./html.js";
 import store from "./admin/store.js";
 import App from "./admin/components/App.js";
+import { fetchAlarms, fetchSounds } from "./admin/actions.js";
 
-// Render our App.
-ReactDOM.render(html`
+fetch('/client/js/tzdata.json')
+  .then(response => response.json())
+  .then(tzdata => moment.tz.load(tzdata))
+  .then(() => {
+    // Render our App.
+    ReactDOM.render(html`
   <${ReactRedux.Provider} store=${store}>
     <${App} />
   <//>`,
-  document.getElementById("admin-root")
-);
+      document.getElementById("admin-root")
+    );
 
-// Initialize our state by dispatching an action to request it from the server.
-import { fetchAlarms, fetchSounds } from "./admin/actions.js";
-store.dispatch(fetchAlarms())
-store.dispatch(fetchSounds())
+    // Initialize our state by dispatching an action to request it from the server.
+    store.dispatch(fetchAlarms())
+    store.dispatch(fetchSounds())
+  });
